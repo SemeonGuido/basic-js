88f019943bc37aaea253dc8d0524bc28bb338424
Semeon <55506520+SemeonGuido@users.noreply.github.com>
2020-03-12 03:07:12 +0300

    refactor: add solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,14 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let nameTeam = [];
+  if (Array.isArray(members) === false) {
+    return false;
+  } else {
+    for (let i = 0; i < members.length; i++) {
+      if (typeof members[i] === 'string') {
+        let firstLetter = members[i].match(/[A-Z]/i);
+        nameTeam.push(firstLetter[0].toUpperCase());
+      }
+    }
+  }
+  return nameTeam.sort().join('');
 };
