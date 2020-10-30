4bc11427573c8195c9581ea110d95602f2e02fa2
Semeon Guido <Semeon.Guido@gmail.com>
2020-10-30 15:50:13 +0300

    feat: carbon task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,16 @@
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  let activ;
+
+  if (typeof (sampleActivity) == 'string') {
+    activ = Number.parseFloat(sampleActivity);
+  } else {
+    activ = false;
+  }
+
+  if (!activ || activ > 15 || activ < 0) return false;
+
+  return Math.ceil(Math.log(MODERN_ACTIVITY / activ) * HALF_LIFE_PERIOD / 0.693);
 };
